8b022ee3deb77e6c8b2717f1271ebafd2d5d3344
Willie Zutz <boarder2@bit-shift.com>
2021-03-14 19:45:18 +0000

    It's alive

--- auth.py
+++ auth.py
@@ -1,30 +1,55 @@
 import datetime
+import uuid
+from enum import Enum
+
+from discord.abc import User
+from discord.guild import Guild
+
+class AuthSessionState(Enum):
+	NONE = 1,
+	NEED_USERNAME = 2,
+	NEED_TOKEN = 3
 
 class AuthSession():
-	def __init__(self, discord_username: str, discord_guild: str) -> None:
-		self.discord_username = discord_username
+	def __init__(self, discord_username: User, discord_guild: Guild, state: AuthSessionState = AuthSessionState.NEED_USERNAME) -> None:
+		self.discord_user = discord_username
 		self.discord_guild = discord_guild
+		self.state = state
 
-	discord_username: str
-	discord_guild: str
+	discord_user: User
+	discord_guild: Guild
 	shack_name: str
 	auth_token: str
 	expire_time: datetime
+	state: AuthSessionState = AuthSessionState.NEED_USERNAME
 
 class AuthService:
 	sessions = dict()
 
-	def __init__(self) -> None:
-		pass
+	def get_session_state(self, discord_user: User) -> AuthSessionState:
+		session = self.sessions.get(discord_user)
+		if session:
+			return session.state
+		return AuthSessionState.NONE
 
-	def start_session(self, discord_username: str, discord_guild: str):
-		self.sessions[discord_username] = AuthSession(discord_username, discord_guild)
+	def set_shack_username_get_token(self, discord_user: User, shack_username: str) -> str:
+		session : AuthSession = self.sessions[discord_user]
+		if session:
+			session.shack_name = shack_username
+			session.auth_token = str(uuid.uuid4()).upper()[:6]
+			session.state = AuthSessionState.NEED_TOKEN
+			self.sessions[discord_user] = session
+			return session.auth_token
+
+	def start_session(self, discord_user: User, discord_guild: Guild):
+		self.sessions[discord_user] = AuthSession(discord_user, discord_guild)
 	
-	def get_token(self, discord_username: str, shack_name: str) -> str:
-		if (discord_username in self.sessions == False):
-			print('no session')
-			return
+	def match_token(self, discord_user: User, token: str) -> bool:
+		if (discord_user in self.sessions == False):
+			return False
 		
-		session : AuthSession = self.sessions.get(discord_username)
-		session.shack_name = shack_name
-		return 'TOKEN'
+		session : AuthSession = self.sessions.get(discord_user)
+		return session.auth_token == token.strip()
+
+	def remove_session(self, discord_user: User):
+		self.sessions.pop(discord_user)
--- discord_bot.py
+++ discord_bot.py
@@ -2,7 +2,8 @@ import discord
 import logging
 
 from discord import Message
-from auth import AuthService
+from discord.enums import ChannelType
+from auth import AuthService, AuthSessionState
 
 class DiscordBot(discord.Client):
 	def __init__(self, auth_svc: AuthService):
@@ -13,5 +14,28 @@ class DiscordBot(discord.Client):
 	async def on_ready(self):
 		logging.info("Logged in as {0}".format(self.user))
 	async def on_message(self, message: Message):
-		self.auth_svc.start_session(message.author, message.guild.name)
-		logging.info("{0.author}:{0.author.guild.name} - {0.content} ".format(message))
+		if message.author == self.user: return
+		logging.info(message)
+
+		if message.channel.type == ChannelType.text:
+			if message.content == '!auth':
+				self.auth_svc.start_session(message.author, message.guild.name)
+				await message.author.send('What is your shacknews username?')
+				logging.info(f'Starting auth session for {message.author}')
+
+		elif message.channel.type == ChannelType.private:
+			if self.auth_svc.get_session_state(message.author) == AuthSessionState.NEED_USERNAME:
+				token = self.auth_svc.set_shack_username_get_token(message.author, message.content)
+				if token:
+					await message.author.send('Check your shackmessages and reply with the token.')
+					logging.info(f'Generated token {token} for {message.author} with shackname {message.content}')
+
+			elif self.auth_svc.get_session_state(message.author) == AuthSessionState.NEED_TOKEN:
+				if self.auth_svc.match_token(message.author, message.content):
+					await message.author.send('Successfully authenticated!')
+					self.auth_svc.remove_session(message.author)
+					logging.info(f'Successfully authenticated {message.author}')
+				else:
+					await message.author.send('Token didn\'t match please try again.')
+			else:
+				await message.author.send('Not sure what to do here. You don\'t have an active authentication session. Type !auth in the server you want to authenticate against.')
